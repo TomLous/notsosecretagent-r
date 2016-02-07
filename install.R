3c5c4121c50f12592ba0f5de4b133ef54c652a7a
Tom Lous <tomlous@gmail.com>
2016-02-07 08:52:20 +0100

    repos

--- install.R
+++ install.R
@@ -1,10 +1,10 @@
-install.packages("devtools")
-install.packages("rmongodb")
-install.packages("RTextTools")
-install.packages("e1071")
-install.packages("tm")
-install.packages("plyr")
-install.packages("magrittr")
+#install.packages("devtools")
+install.packages("rmongodb", repos="http://cran.rstudio.com/")
+install.packages("RTextTools", repos="http://cran.rstudio.com/")
+install.packages("e1071", repos="http://cran.rstudio.com/")
+install.packages("tm", repos="http://cran.rstudio.com/")
+install.packages("plyr", repos="http://cran.rstudio.com/")
+install.packages("magrittr", repos="http://cran.rstudio.com/")
 
 # wget http://cran.r-project.org/src/contrib/Archive/tm/tm_0.5-10.tar.gz
 # R CMD INSTALL tm_0.5-10.tar.gz
